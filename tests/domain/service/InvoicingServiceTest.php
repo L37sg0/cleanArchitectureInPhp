130fb46e3a56f6fb0f7789e352f222dd79d2ae2b
l37sg0 <petar_1v@abv.bg>
2022-10-20 18:58:28 +0300

    [#24]  - tested InvoicingService

--- tests/domain/service/InvoicingServiceTest.php
+++ tests/domain/service/InvoicingServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\domain\service;
 
 use L37sg0\Architecture\Domain\Entity\Invoice;
+use L37sg0\Architecture\Domain\Entity\Order;
 use L37sg0\Architecture\Domain\Factory\InvoiceFactory;
 use L37sg0\Architecture\Domain\Repository\OrderRepositoryInterface;
 use L37sg0\Architecture\Domain\Service\InvoicingService;
@@ -19,12 +20,38 @@ class InvoicingServiceTest extends TestCase
     }
 
     public function testCanQueryRepositoryForUnInvoicedOrders() {
-        $invoice = new Invoice();
-        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
-        $orderRepository->method('getUnInvoicedOrders')->willReturn([$invoice]);
+        $order1 = new Order();
+        $order2 = new Order();
         $invoiceFactory = new InvoiceFactory();
+
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $orderRepository->method('getUnInvoicedOrders')->willReturn([$order1, $order2]);
         $service = new InvoicingService($orderRepository, $invoiceFactory);
 
-        $this->assertEquals([$invoice], $service->generateInvoices());
+        foreach ($service->generateInvoices() as $invoice) {
+            $this->assertInstanceOf(Invoice::class, $invoice);
+        }
     }
+
+    public function testCanReturnInvoiceForEachUnInvoicedOrder() {
+        $order1 = new Order();
+        $order1->setTotal(400);
+        $order2 = new Order();
+        $order2->setTotal(500);
+        $orders = [$order1, $order2];
+        $invoiceFactory = new InvoiceFactory();
+
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $orderRepository->method('getUnInvoicedOrders')->willReturn($orders);
+        $service = new InvoicingService($orderRepository, $invoiceFactory);
+
+        foreach ($service->generateInvoices() as $key => $invoice) {
+            $this->assertInstanceOf(Invoice::class, $invoice);
+            $this->assertEquals($orders[$key]->getTotal(), $invoice->getTotal());
+        }
+
+
+    }
+
+
 }

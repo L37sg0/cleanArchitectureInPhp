c8f4ac34a9f545799de3b077ba8e52ab37781387
l37sg0 <petar_1v@abv.bg>
2022-10-23 22:32:57 +0300

    [#24]  - Added and tested test for InvoiceHydrator

--- tests/hydrator/InvoiceHydratorTest.php
+++ tests/hydrator/InvoiceHydratorTest.php
@@ -4,6 +4,7 @@ namespace Tests\hydrator;
 
 use DateTime;
 use L37sg0\Architecture\Domain\Entity\Invoice;
+use L37sg0\Architecture\Domain\Entity\Order;
 use L37sg0\Architecture\Domain\Repository\OrderRepositoryInterface;
 use L37sg0\Architecture\Persistence\Hydrator\ClassMethodsHydrator;
 use L37sg0\Architecture\Persistence\Hydrator\InvoiceHydrator;
@@ -33,8 +34,69 @@ class InvoiceHydratorTest extends TestCase
         $invoice->setInvoiceDate(new DateTime());
 
         $data = $hydrator->extract($invoice);
-        var_dump($invoice,$data);exit();
 
         $this->assertEquals($invoice->getInvoiceDate()->format('Y-m-d'), $data['invoice_date']);
+    }
+
+    public function testCanExtractOrderObject() {
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $wrappedHydrator = new ClassMethodsHydrator();
+        $hydrator = new InvoiceHydrator($wrappedHydrator, $orderRepository);
+
+        $invoice = new Invoice();
+        $invoice->setOrder((new Order())->setId(14));
+
+        $data = $hydrator->extract($invoice);
+
+        $this->assertEquals($invoice->getOrder()->getId(), $data['order_id']);
+    }
+
+    public function testCanPerformSimpleHydrationOnObject() {
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $wrappedHydrator = new ClassMethodsHydrator();
+        $hydrator = new InvoiceHydrator($wrappedHydrator, $orderRepository);
+
+        $data['total'] = 300.14;
+        $invoice = $hydrator->hydrate($data, new Invoice());
+
+        $this->assertEquals($data['total'], $invoice->getTotal());
+    }
+
+    public function testCanHydrateDateTimeObject() {
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $wrappedHydrator = new ClassMethodsHydrator();
+        $hydrator = new InvoiceHydrator($wrappedHydrator, $orderRepository);
+
+        $data['invoice_date'] = '2022-10-23';
+        $invoice = $hydrator->hydrate($data, new Invoice());
+
+        $this->assertInstanceOf(DateTime::class, $invoice->getInvoiceDate());
+        $this->assertEquals($data['invoice_date'], $invoice->getInvoiceDate()->format('Y-m-d'));
+    }
+
+    public function testCanHydrateOrderEntityOnInvoice()  {
+        $order = (new Order())->setId(500);
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $orderRepository->method('getById')->willReturn($order)->with(500);
+        $wrappedHydrator = new ClassMethodsHydrator();
+        $hydrator = new InvoiceHydrator($wrappedHydrator, $orderRepository);
+
+        $data['order_id'] = 500;
+        $invoice = $hydrator->hydrate($data, new Invoice());
+
+        $this->assertEquals($order, $invoice->getOrder());
+    }
+
+    public function testCanHydrateEmbeddedOrderData() {
+        $orderRepository = $this->createMock(OrderRepositoryInterface::class);
+        $wrappedHydrator = new ClassMethodsHydrator();
+        $hydrator = new InvoiceHydrator($wrappedHydrator, $orderRepository);
+
+        $data['order']['id'] = 20;
+        $invoice = new Invoice();
+
+        $hydrator->hydrate($data, $invoice);
+
+        $this->assertEquals($data['order']['id'], $invoice->getOrder()->getId());
     }
 }
